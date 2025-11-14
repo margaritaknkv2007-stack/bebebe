286c709bae55fbd74daf7c8a96dde4297f5f6380
margaritaknkv2007-stack <margarita.knkv2007@gmail.com>
2025-11-14 13:09:24 +0300

    готовое задание

--- Лаб_2.2.py
+++ Лаб_2.2.py
@@ -2,7 +2,10 @@ s = input("Введите числа через пробел:")
 num = s.split()
 sum = 0
 for i in num:
-    num = int(i)
-    if num % 3 == 0:
-        sum += num
+    try:
+        num = int(i)
+        if num % 3 == 0:
+            sum += num
+    except ValueError:
+        print("Ошибка: '{i}' не является числом")
 print(sum)
